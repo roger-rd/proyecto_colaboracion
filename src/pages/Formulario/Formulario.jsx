464e0b5c3ce3a2ae23e20eb99d20ea9884d10334
Luis Alejandro Peña Rivas <luisrivas0311@gmail.com>
2023-08-12 17:07:38 -0400

    arreglo button formulario

--- src/pages/Formulario/Formulario.jsx
+++ src/pages/Formulario/Formulario.jsx
@@ -54,12 +54,12 @@ export default function Formulario() {
     <div className="form-check">
       <input className="form-check-input" type="checkbox" id="gridCheck"/>
       <label className="form-check-label" for="gridCheck">
-        Registrar
+        Check me out
       </label>
     </div>
   </div>
   <div className="col-12">
-    <button type="submit" className="btn btn-primary">Sign in</button>
+    <button type="submit" className="btn btn-primary">Registrar</button>
   </div>
 </form>
     </div>
