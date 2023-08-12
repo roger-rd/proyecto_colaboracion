199f0404f93cb8328823fe103d58d5f62f56bfe9
Luis Alejandro Peña Rivas <luisrivas0311@gmail.com>
2023-08-12 16:57:41 -0400

    arreglo login2

--- src/pages/Formulario/Formulario.jsx
+++ src/pages/Formulario/Formulario.jsx
@@ -12,39 +12,44 @@ export default function Formulario() {
     <label for="inputPassword4" className="form-label">Password</label>
     <input type="password" className="form-control" id="inputPassword4"/>
   </div>
-  <div className="col-12">
-    <label for="inputAddress" className="form-label">Address</label>
-    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"/>
+  <div class="row g-3">
+  <div class="col">
+    <input type="text" class="form-control" placeholder="First name" aria-label="First name"/>
   </div>
+  <div class="col">
+    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name"/>
+  </div>
+</div>
+<div class="col-md-2">
+    <label for="inputZip" class="form-label">Cintura</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Busto</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Altura</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  <div class="col-md-2">
+    <label for="inputZip" class="form-label">Peso</label>
+    <input type="text" class="form-control" id="inputZip"/>
+  </div>
+  
   
   <div className="col-md-2">
     <label for="inputState" className="form-label">State</label>
     <select id="inputState" className="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
+      <option selected> Tu Cuerpo.</option>
+      <option>Triangilo Invertido</option>
+      <option>Reloj de arena</option>
+      <option>Triangulo</option>
+      <option>Rectangular</option>
+      <option>Ovalado</option>
     </select>
   </div>
-  <div className="col-md-2">
-    <label for="inputState" className="form-label">State</label>
-    <select id="inputState" className="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div className="col-md-2">
-    <label for="inputState" className="form-label">State</label>
-    <select id="inputState" className="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
-  <div className="col-md-2">
-    <label for="inputState" className="form-label">State</label>
-    <select id="inputState" className="form-select">
-      <option selected>Choose...</option>
-      <option>...</option>
-    </select>
-  </div>
+  
   <div className="col-12">
     <div className="form-check">
       <input className="form-check-input" type="checkbox" id="gridCheck"/>
